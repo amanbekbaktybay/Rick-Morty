019eb5cef35c731260b2b1599b96c2a5d9be2b4a
amanbekbaktybay <baktibai180403@gmail.com>
2021-10-06 21:46:06 +0600

    add CharacterProfile page(end makeup)

--- rick-and-morty/src/components/ui/base/Episode.jsx
+++ rick-and-morty/src/components/ui/base/Episode.jsx
@@ -1,15 +1,20 @@
 import "../../../styles/components/Episode.scss";
+import {ArrowMore} from "./ArrowMore";
 
 export function Episode(props){
 
     return (
         <div className="episode">
-            <img src={props.link} alt="" className="episode__img"/>
-            <div className="episode__info">
-                <p className="episode__series">Серия {props.series}</p>
-                <h2 className="episode__name">{props.name}</h2>
-                <p className="episode__date">{props.date}</p>
+            <div className="episode__wrapper">
+                <img src={props.link} alt="" className="episode__img"/>
+                <div className="episode__info">
+                    <p className="episode__series">Серия {props.series}</p>
+                    <h2 className="episode__name">{props.name}</h2>
+                    <p className="episode__date">{props.date}</p>
+                </div>
             </div>
+
+            {props.isMoreButton?<ArrowMore></ArrowMore>:null}
         </div>
     );
 }
--- rick-and-morty/src/pages/CharacterProfile.jsx
+++ rick-and-morty/src/pages/CharacterProfile.jsx
@@ -1,27 +1,86 @@
 import "../styles/components/CharacterProfile.scss";
+import {Episode} from "../components/ui/base/Episode";
 
 export function CharacterProfile(props){
 
     return(
         <div className="container">
             <div className="character-profile">
-                <div className="character-profile__person">
-                    <div className="character-profile__ava">
-                        <div className="character-profile__ava-bg">
-                            <div className="character-profile__ava-bg-wrapper">
+                <div className="person">
+                    <div className="person__ava">
+                        <div className="person__ava-bg">
+                            <div className="person__ava-bg-wrapper">
                                 <img src="assets/images/rick.png" alt=""/>
                             </div>
                         </div>
                     </div>
-                    <div className="character-profile__personal-data">
-                        <h1 className="character-profile__fullName">Рик Санчез</h1>
-                        <p className="character-profile__status">Живой</p>
+                    <div className="person__personal-data">
+                        <div className="person__fullName-status">
+                            <h1 className="person__fullName">Рик Санчез</h1>
+                            <p className="person__status">Живой</p>
+                        </div>
 
-                        <p className="character-profile__description">
+                        <p className="person__description">
                             Главный протагонист мультсериала «Рик и Морти». Безумный ученый, чей алкоголизм, безрассудность
                             и социопатия заставляют беспокоиться семью его дочери.
                         </p>
 
+                        <div className="person__gender-Race">
+                            <span>
+                                <h5 className="person__heading">Пол</h5>
+                                <p className="person__after-heading">Мужской</p>
+                            </span>
+                            <span>
+                                <h5 className="person__heading">Расса</h5>
+                                <p className="person__after-heading">Человек</p>
+                            </span>
+                        </div>
+
+                        <div className="person__place-birth">
+                            <h5 className="person__heading">Место рождение</h5>
+                            <p className="person__after-heading">Земля С-137</p>
+                        </div>
+                        <div className="person__location">
+                            <h5 className="person__heading">Местоположение</h5>
+                            <p className="person__after-heading">Земля(Измерение подменны)</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="episodes-visited">
+                    <div className="episodes-visited__title">
+                        <h2>Эпизоды</h2>
+                        <p>Все эпизоды</p>
+                    </div>
+                    <div className="episodes-visited__block">
+                        <Episode
+                            link="assets/images/rick.png"
+                            name={"qwe"}
+                            series={"qeqw"}
+                            date={"Qeqwe"}
+                            key={"qeqw"}
+                            isMoreButton={true}
+                        /><Episode
+                            link="assets/images/rick.png"
+                            name={"qwe"}
+                            series={"qeqw"}
+                            date={"Qeqwe"}
+                            key={"qeqw"}
+                            isMoreButton={true}
+                        /><Episode
+                            link="assets/images/rick.png"
+                            name={"qwe"}
+                            series={"qeqw"}
+                            date={"Qeqwe"}
+                            key={"qeqw"}
+                            isMoreButton={true}
+                        /><Episode
+                            link="assets/images/rick.png"
+                            name={"qwe"}
+                            series={"qeqw"}
+                            date={"Qeqwe"}
+                            key={"qeqw"}
+                            isMoreButton={true}
+                        />
                     </div>
                 </div>
             </div>
